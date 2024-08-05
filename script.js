1a38c9d4e3f21f71399744e6995e225f8c79ecab
Cynthia Freund <cfreund@creditacceptance.com>
2024-08-05 00:32:09 -0400

    added for loop and table

--- script.js
+++ script.js
@@ -4,35 +4,36 @@
 3. create user input prompt for two flavors and console log the input
 4. split the users input into an array of strings
 5. create a function to loop through the array and get a count of each flavor in the array
-
+    I want the function to loop through the [array] and check each index of the array, 
+    I want the function to check if the flavor index matches any of the other indexes,
+    if not, count it as 1 else count the flavor as +1 to the existing count.
 */
 
 //prompt user for yogurt flavors and write to the console
 const userInput = prompt(
-    "Please enter the flavors of each frozen yogurt sold today, separated by a comma:"
+  "Please enter the flavors of each frozen yogurt sold today, separated by a comma:", "vanilla,choc,mango,vanilla,vanilla,choc,mint,cherry"
   );
   console.log(userInput);
 
-//take user input and create a string array of the input values
-  const stringArray = userInput.split(",");
-  console.log(stringArray);
+//take user input and create an array of flavors from the input values
+const flavorArray = userInput.split(",");
+  console.log(flavorArray);
 
-//function to loop through and get a count of each flavor sold that day
-/*function countFlavors(stringArray) {
-    let flavorCount = {};
-    for(let i = 0; i < stringArray.length; i++) {
-        if(!flavorCount[stringArray[i]]) {
-            flavorCount[stringArray[i]] = 1;
-        } else {
-            flavorCount[stringArray[i]] += 1;
-        }
+// loop through and get a count of each flavor sold that day
+let flavorCount = {};
+    
+for(let i = 0; i < flavorArray.length; i++) {
+    if(flavorCount[flavorArray[i]]) {
+        flavorCount[flavorArray[i]] += 1;
+    } else {
+        flavorCount[flavorArray[i]] = 1;
     }
-    return flavorCount;
 }
-    console.log(flavorCount);*/
+console.table(flavorCount);
 
-const flavorCounts = {};
-stringArray.forEach((i) => {
-    flavorCounts[i] = (flavorCounts[i] || 0) + 1;
-});
-console.log(flavorCounts);
+
+
+
+
+
+ 
