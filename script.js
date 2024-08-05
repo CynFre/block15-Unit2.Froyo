bb624306abe2e6fc25304efbb51b2cd9e571eb73
Cynthia Freund <cfreund@creditacceptance.com>
2024-08-05 00:48:06 -0400

    cleaned up code

--- script.js
+++ script.js
@@ -3,15 +3,16 @@
 2. add description to ReadMe file
 3. create user input prompt for two flavors and console log the input
 4. split the users input into an array of strings
-5. create a function to loop through the array and get a count of each flavor in the array
+5. loop through the array and get a count of each flavor in the array
     I want the function to loop through the [array] and check each index of the array, 
     I want the function to check if the flavor index matches any of the other indexes,
     if not, count it as 1 else count the flavor as +1 to the existing count.
+6. create the table of results
 */
 
 //prompt user for yogurt flavors and write to the console
 const userInput = prompt(
-  "Please enter the flavors of each frozen yogurt sold today, separated by a comma:", "vanilla,choc,mango,vanilla,vanilla,choc,mint,cherry"
+  "Please enter the flavors of each frozen yogurt sold today, separated by a comma:"
   );
   console.log(userInput);
 
@@ -28,7 +29,8 @@ for(let i = 0; i < flavorArray.length; i++) {
     } else {
         flavorCount[flavorArray[i]] = 1;
     }
-}
+ }
+console.log(flavorCount);
 console.table(flavorCount);
 
 
